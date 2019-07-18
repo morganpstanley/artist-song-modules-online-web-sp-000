493d2d69585b59adf9ac9dd49934466cf8c9927a
Morgan Stanley <morganpstanley@gmail.com>
2019-07-18 05:19:15 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,29 +1,20 @@
 require 'pry'
 
 class Song
+  extend Memorable::ClassMethods
+  extend Findable
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  #def initialize
+  #  self.class.all << self
+#  end
 
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def artist=(artist)
